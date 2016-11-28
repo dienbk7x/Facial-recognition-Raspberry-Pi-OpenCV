0379c0a610ec91b2386754f98f7eae93bc8bb823
Monsieur Vechai <johnkimdinh@gmail.com>
2016-11-28 01:21:01 +0100

    Update Watson_face_recognition.py

--- Watson_face_recognition.py
+++ Watson_face_recognition.py
@@ -12,12 +12,12 @@ from watson_developer_cloud import VisualRecognitionV3
 camera = cv2.VideoCapture(0)
 
 if __name__ == '__main__':
-    # Initialize camer and box.'	
+    # Initialize camera
     ret, frame = camera.read()
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     result = face.detect_single(image)
     
-    visual_recognition = VisualRecognitionV3('2016-05-20', api_key='a4f1c9fe8d9015cedbac7e3ca2c4dd8b82677008')
+    visual_recognition = VisualRecognitionV3('2016-05-20', api_key='YOUR_API_KEY')
     photo = cv2.imwrite('capture'+'.png',frame)
     path_of_photo = str(os.path.abspath('capture.png'))
     with open(path_of_photo, 'rb') as image_file:
